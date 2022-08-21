7f287d353bbaa6853a041ba31138b464d9e9947a
tWizT3ddreaMr <tWizt3d.dreamr0729@gmail.com>
2022-08-21 17:04:10 -0500

    Filterall Removal

--- src/main/java/me/tWizT3d_dreaMr/ShopAddon/main.java
+++ src/main/java/me/tWizT3d_dreaMr/ShopAddon/main.java
@@ -16,7 +16,6 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
@@ -26,8 +25,6 @@ import com.sk89q.worldguard.protection.ApplicableRegionSet;
 import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 import com.snowgears.shop.Shop;
-import com.snowgears.shop.shop.ShopType;
-
 import me.tWizT3d_dreaMr.ShopAddon.Gui.Guis;
 import me.tWizT3d_dreaMr.ShopAddon.Listeners.CheckOn;
 import me.tWizT3d_dreaMr.ShopAddon.Listeners.ShopLogging;
@@ -200,54 +197,6 @@ public boolean onCommand(CommandSender sender, Command command, String label, St
 			  NotificationCommand.Command((Player)sender, args);
 	
 		  return true;
-	  }
-	  if(command.getName().equalsIgnoreCase("filterall")) {
-
-		  if((sender instanceof Player)) {
-			  sender.sendMessage("You cant");
-			  return true;
-		  }
-		  sender.sendMessage("Running");
-		  Bukkit.getScheduler().runTaskAsynchronously(plugin, new Runnable() {
-			    @Override
-			    public void run() {
-			        File fileDirectory = new File( "/plugins/Shop/Data");
-			        if (!fileDirectory.exists())
-			            return;
-
-			        ConfigurationSection ShopConfig;
-			        YamlConfiguration ShopConfig2;
-			        for (File file : fileDirectory.listFiles()) {
-			            if (file.isFile()) {
-			                if (file.getName().endsWith(".yml")){
-			                	String uuid=file.getName().replace(".yml","");
-			 			        System.out.println("File "+uuid+ ".yml");
-			                	ShopConfig=YamlConfiguration.loadConfiguration(file)
-			                			.getConfigurationSection("Shops."+uuid);
-			                	ShopConfig2=YamlConfiguration.loadConfiguration(file);
-			                	for(String s: ShopConfig.getKeys(false)) {
-			                		ShopType st=ShopType.valueOf(ShopConfig.getString(s+".type").toUpperCase());
-			                		Double price=ShopConfig.getDouble(s+".price");
-			                		int amount=ShopConfig.getInt(s+".amount");
-			                		ItemStack is=ShopConfig.getItemStack(s+".item");
-			                		MatchType type=main.getCreationCheck().test(is, price, amount, st);
-			            	    	if(!(type==null||type.getType().equalsIgnoreCase("WhiteList"))) {
-			            	    		ShopConfig2.set(s, null);
-			            	    	}
-			                	}
-				                try {
-									ShopConfig2.save(file);
-								} catch (IOException e) {
-									// TODO Auto-generated catch block
-									e.printStackTrace();
-								}
-			                }
-			            }
-			        }
-			        
-			        System.out.println("done");
-			    }
-			});
 	  }
 	  if(command.getName().equalsIgnoreCase("SASQLConversion")) {
 
